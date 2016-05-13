43a0def1a60b476ec19cd06aa04088a75491de10
Dave Conway-Jones <conway@uk.ibm.com>
2016-05-13 10:03:00 +0100

    Update index and gulp to use latest Raphael
    closes #9

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,58 @@
-var 
-  gulp = require('gulp'),
-
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  templateCache = require('gulp-angular-templatecache'),
-  ghtmlSrc = require('gulp-html-src'),
-  gutil = require('gulp-util'),
-  minifyCss = require('gulp-minify-css'),
-  gulpif = require('gulp-if'),
-  htmlreplace = require('gulp-html-replace'),
-  minifyHTML = require('gulp-minify-html'),
-  
-  path = require('path'),
-  spawn = require('child_process').spawn,
-  streamqueue = require('streamqueue');
+var
+    gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    templateCache = require('gulp-angular-templatecache'),
+    ghtmlSrc = require('gulp-html-src'),
+    gutil = require('gulp-util'),
+    minifyCss = require('gulp-minify-css'),
+    gulpif = require('gulp-if'),
+    htmlreplace = require('gulp-html-replace'),
+    minifyHTML = require('gulp-minify-html'),
+    path = require('path'),
+    spawn = require('child_process').spawn,
+    streamqueue = require('streamqueue');
 
 gulp.task('build', ['icon', 'js', 'css', 'index', 'fonts']);
 
 gulp.task('publish', ['build'], function (done) {
-  spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
-});    
-    
+    spawn('npm', ['publish'], { stdio:'inherit' }).on('close', done);
+});
+
 gulp.task('index', function() {
-  return gulp.src('src/index.html')
+    return gulp.src('src/index.html')
     .pipe(htmlreplace({
         'css': 'css/app.min.css',
         'js': 'js/app.min.js'
     }))
-    .pipe(minifyHTML({spare: true, quotes: true}))
+    .pipe(minifyHTML({spare:true, quotes:true}))
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('icon', function() {
-   return gulp.src('src/icon.png').pipe(gulp.dest('dist/')); 
+    return gulp.src('src/icon.png').pipe(gulp.dest('dist/'));
 });
 
 gulp.task('fonts', function() {
-   return gulp.src('node_modules/font-awesome/fonts/*').pipe(gulp.dest('dist/fonts/')); 
+    return gulp.src('node_modules/font-awesome/fonts/*').pipe(gulp.dest('dist/fonts/'));
 });
-    
+
 gulp.task('js', function () {
-  var scripts = gulp.src('src/index.html')
-    .pipe(ghtmlSrc({getFileName: getFileName.bind(this, 'src')}));
-    
-  var templates = gulp.src(['src/**/*.html', '!src/index.html'])
+    var scripts = gulp.src('src/index.html')
+    .pipe(ghtmlSrc({getFileName:getFileName.bind(this, 'src')}));
+
+    var templates = gulp.src(['src/**/*.html', '!src/index.html'])
     .pipe(minifyHTML({spare: true, quotes: true}))
-    .pipe(templateCache('templates.js',  {root: '', module: 'ui'}));
-    
-  return  streamqueue({ objectMode: true }, scripts, templates)
+    .pipe(templateCache('templates.js', {root:'', module:'ui'}));
+
+    return streamqueue({ objectMode:true }, scripts, templates)
     .pipe(gulpif(/[.]min[.]js$/, gutil.noop(), uglify()))
     .pipe(concat('app.min.js'))
     .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('css', function () {
-  return gulp.src('src/index.html')
+    return gulp.src('src/index.html')
     .pipe(ghtmlSrc({getFileName: getFileName.bind(this, 'href'), presets: 'css'}))
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(concat('app.min.css'))
@@ -63,8 +61,9 @@ gulp.task('css', function () {
 
 var vendorPrefix = "vendor/";
 function getFileName(attr, node) {
-  var file = node.attr(attr);
-  if (file.indexOf(vendorPrefix) === 0)
-    file = path.join("..", "node_modules", file.substr(vendorPrefix.length));
-  return file;
+    var file = node.attr(attr);
+    if (file.indexOf(vendorPrefix) === 0) {
+        file = path.join("..", "node_modules", file.substr(vendorPrefix.length));
+    }
+    return file;
 }
--- nodes/ui_text.js
+++ nodes/ui_text.js
@@ -4,39 +4,34 @@ module.exports = function(RED) {
     function TextNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        
         var tab = RED.nodes.getNode(config.tab);
         var group = RED.nodes.getNode(config.group);
-        if (!tab || !group) return;
-        
+        if (!tab || !group) { return; }
+
         var done = ui.add({
             emitOnlyNewValues: false,
-            node: node, 
-            tab: tab, 
+            node: node,
+            tab: tab,
             group: group,
             control: {
                 type: 'text',
                 label: config.label,
                 order: config.order,
                 format: config.format,
-				width: config.width || 6,
-				height: config.height || 1
+                width: config.width || 6,
+                height: config.height || 1
             },
             beforeEmit: function(msg, value) {
                 var properties = Object.getOwnPropertyNames(msg).filter(function (p) {return p[0] != '_';});
                 var clonedMsg = { };
-                
                 for (var i=0; i<properties.length; i++) {
                     var property = properties[i];
                     clonedMsg[property] = msg[property];
                 }
-                
                 return { msg: clonedMsg };
             }
         });
-
         node.on("close", done);
     }
-
     RED.nodes.registerType("ui_text", TextNode);
 };
