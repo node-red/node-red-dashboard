199dcc94abd24943f04e7818bebadbac0706e72c
Joe Pavitt <joepavit@uk.ibm.com>
2016-05-13 16:09:17 +0100

    Fix #6 - Simplify the positioning calculation and include the padding when working out the avialable space for a given line

--- src/components/ui-card-panel/ui-masonry.js
+++ src/components/ui-card-panel/ui-masonry.js
@@ -1,62 +1,65 @@
 /* global angular */
 angular.module('ui').directive('uiMasonry', ['$window',
-    function($window) {
-        return {
-            restrict: 'A',
-            controller: MasonryController,
-            link: function (scope, element, attrs, controller) {
-                controller.init(element);
-                angular.element($window).on('resize', function () {
-                    controller.refreshLayout();
-                });
-            }
-        };
+    function ($window) {
+		return {
+			restrict: 'A',
+			controller: MasonryController,
+			link: function (scope, element, attrs, controller) {
+				controller.init(element);
+				angular.element($window).on('resize', function () {
+					controller.refreshLayout();
+				});
+			}
+		};
     }]);
 
 MasonryController.$inject = ['uiSizes', '$timeout'];
+
 function MasonryController(sizes, $timeout) {
-    var ctrl = this;
-    var root;
-    ctrl.init = function (rootElement) {
-        root = rootElement;
-        root.addClass('masonry-container');
-    };
+	var ctrl = this;
+	var root;
+	ctrl.init = function (rootElement) {
+		root = rootElement;
+		root.addClass('masonry-container');
+	};
 
-    var refreshInProgress = false;
-    ctrl.refreshLayout = function() {
-        if (refreshInProgress) return;
-        refreshInProgress = true;
-        $timeout(function() {
-            refreshSizes();
-            refreshInProgress = false;
-        }, 0);
-    };
+	var refreshInProgress = false;
+	ctrl.refreshLayout = function () {
+		if (refreshInProgress) return;
+		refreshInProgress = true;
+		$timeout(function () {
+			refreshSizes();
+			refreshInProgress = false;
+		}, 0);
+	};
 
-    function refreshSizes() {
-        var children = root.children();
-        var availableWidth = root.width();
-		var sum = 0, c = 0;
+	function refreshSizes() {
+		var children = root.children();
+		var availableWidth = root.width();
+		var sum = 0,
+			c = 0;
 		while (sum < availableWidth && c < (children.length)) {
 			// how many groups can fit into one row/screen width?
 			sum += getPxWidth(children[c]);
+			sum += sizes.px;
 			c++;
 		}
-        var firstRow = Math.max(1, Math.min(children.length, (sum > availableWidth) ? (c-1) : c));
+		sum -= sizes.px;
+		var firstRow = Math.max(1, Math.min(children.length, (sum > availableWidth) ? (c - 1) : c));
 
 		var groupsWidth = 0;
-        for (var i=0; i<firstRow; i++) {
+		for (var i = 0; i < firstRow; i++) {
 			groupsWidth += getPxWidth(children[i]);
 		}
 		groupsWidth += (sizes.px * (children.length - 1)); // add gap between groups
-
-        var leftPadding = Math.max(0, (availableWidth - groupsWidth) / 2);
+		var leftPadding = Math.max(0, (availableWidth - groupsWidth) / 2);
 		leftPadding = (getMaxWidth(children) + leftPadding > availableWidth) ? sizes.px : leftPadding;
 
-        var maxy = 0;
-        children.each(function (c) {
-            var child = $(this);
-            var x = 0, y = sizes.gy;
-            for (var j = 0; j < maxy; j++) {
+		var maxy = 0;
+		children.each(function (c) {
+			var child = $(this);
+			var x = 0, y = sizes.gy;
+			for (var j = 0; j < maxy; j++) {
 				if (j === maxy - 1) {
 					// place the group on a new row
 					x = 0;
@@ -64,39 +67,38 @@ function MasonryController(sizes, $timeout) {
 					break;
 				}
 				var maxx = availableWidth - 2 * leftPadding;
-				var openX = getPxXOffset(children, c, maxx, j); // for the given y, what's the next available x-coordinate?
-				if ( openX >= 0 && (openX + getPxWidth(child)) <= maxx) {
+				var openX = getPxXOffset(children, c, maxx, j); // for the given y, <j>, what's the next available x-coordinate?
+				if (openX >= 0 && (openX + getPxWidth(child)) <= maxx) {
 					y += j;
 					x = openX
 					break;
 				}
 			}
-            child.css({
-                left: leftPadding + x,
-                top: y
-            });
-            child.addClass('visible');
-            maxy = Math.max(maxy, y + child.height() + sizes.gy);
-        });
-        root.css('min-height', maxy + sizes.gy);
-    }
+			child.css({
+				left: leftPadding + x,
+				top: y
+			});
+			child.addClass('visible');
+			maxy = Math.max(maxy, y + child.height() + sizes.gy);
+		});
+		root.css('min-height', maxy + sizes.gy);
+	}
 
-	function getPxWidth (group) {
+	function getPxWidth(group) {
 		var cols = parseInt(angular.element(group).scope().group.header.config.width); // the number of columns defined for this group/child
 		return (cols * sizes.sx) + (sizes.px * 2) + ((cols - 1) * sizes.gx); // the width in px of this group/child
 	}
 
-	function getPxXOffset (children, index, maxx, y) {
+	// calculate the next available x-coordinate in the x-axis for a given y for <child>
+	function getPxXOffset(children, maxindex, maxx, y) {
 		var x = 0;
-		for (var c = 0; c < index; c++) {
-			var child = $(children[c]);
-			// only offset the x if it can't go directly beneath the child
-
+		var child = $(children[maxindex]);
+		// index === the number of children before current child
+		for (var i = 0; i < maxindex; i++) {
+			var c = $(children[i]);
 			// if the child exists at the same <y>
-			if (child.height() + parseInt(child.css('top')) > y) {
-				if (x + $(children[index]).width() >= parseInt(child.css('left'))) {
-					x += child.width() + sizes.px;
-				}
+			if (c.height() + parseInt(c.css('top')) > y) {
+				x += c.width() + sizes.px;
 			}
 		}
 		return x;
@@ -104,7 +106,7 @@ function MasonryController(sizes, $timeout) {
 
 	function getMaxWidth(children) {
 		var max = 0;
-		children.each(function(i, c) {
+		children.each(function (i, c) {
 			var width = getPxWidth(c);
 			max = (width > max) ? width : max;
 		});
