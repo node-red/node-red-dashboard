bca1fbd8963aedf86eb22d184706b4ba791e83bc
Dave Conway-Jones <conway@uk.ibm.com>
2016-05-15 18:05:22 +0100

    let text widget also allow msg.topic to set label

--- src/components/ui-component/ui-component-ctrl.js
+++ src/components/ui-component/ui-component-ctrl.js
@@ -3,16 +3,19 @@
 angular.module('ui').controller('uiComponentController', ['UiEvents', '$interpolate', '$interval',
     function (events, $interpolate, $interval) {
         var me = this;
-        if (typeof me.item.format === "string")
+        if (typeof me.item.format === "string") {
             me.item.getText = $interpolate(me.item.format).bind(null, me.item);
-        
+            if (!me.item.label) { me.item.label = ""; }
+            me.item.getLabel = $interpolate(me.item.label).bind(null, me.item);
+        }
+
         me.init = function() {
             switch (me.item.type) {
                 case 'button':
                     me.buttonClick = function () {
                         me.valueChanged(0);
                     };
-                    
+
                 case 'numeric':
                     var changeValue = function(delta) {
                         if (delta > 0) {
@@ -25,7 +28,7 @@ angular.module('ui').controller('uiComponentController', ['UiEvents', '$interpol
                             }
                         }
                     };
-                    
+
                     var range = me.item.max - me.item.min;
                     var promise;
                     me.periodicChange = function(delta) {
@@ -33,10 +36,10 @@ angular.module('ui').controller('uiComponentController', ['UiEvents', '$interpol
                         var i=0;
                         promise = $interval(function() {
                             i++;
-                            if (i>35) changeValue(Math.sign(delta) * Math.floor(range / 10));
-                            else if (i>25) changeValue(delta*2);
-                            else if (i>15) changeValue(delta);
-                            else if (i>5 && i%2) changeValue(delta);
+                            if (i>35) { changeValue(Math.sign(delta) * Math.floor(range / 10)); }
+                            else if (i>25) { changeValue(delta*2); }
+                            else if (i>15) { changeValue(delta); }
+                            else if (i>5 && i%2) { changeValue(delta); }
                         }, 100);
                     };
                     me.stopPeriodic = function() {
@@ -44,31 +47,31 @@ angular.module('ui').controller('uiComponentController', ['UiEvents', '$interpol
                         me.valueChanged(0);
                     };
                     break;
-                    
+
                 case 'chart':
-                    if (!me.item.value) me.item.value = [];
-                
-                    me.formatTime = function(d) {  
-                        return d3.time.format('%H:%M:%S')(new Date(d));  
+                    if (!me.item.value) { me.item.value = []; }
+
+                    me.formatTime = function(d) {
+                        return d3.time.format('%H:%M:%S')(new Date(d));
                     };
             }
         }
-    
+
         me.valueChanged = function(throttleTime) {
             throttle({
                 id: me.item.id,
                 value: me.item.value
             }, typeof throttleTime === "number" ? throttleTime : 10);
         };
-        
+
         var timer;
         var throttle = function(data, timeout) {
             if (timeout === 0) {
                 events.emit(data);
                 return;
             }
-            
-            if (timer) clearTimeout(timer);
+
+            if (timer) { clearTimeout(timer); }
             timer = setTimeout(function() {
                 timer = undefined;
                 events.emit(data);
