632d7a1c455af8df3cc0b3fe8dcceee4c523ae66
Dave Conway-Jones <conway@uk.ibm.com>
2016-05-24 10:46:49 +0100

    Minor css name consistency fixes
    and add placeholder to optional labels

--- src/services/sizes.js
+++ src/services/sizes.js
@@ -1,12 +1,12 @@
 var sizes = {
     sx: 50, // width of <1> grid square
     sy: 50, // height of <1> grid square
-	cx: 7, // gap between components
-	cy: 7, // gap between components
+    cx: 7, // gap between components
+    cy: 7, // gap between components
     gx: 7, // gap between groups
     gy: 7, // gap between groups
     px: 7, // padding of group's cards
-    py: 7 // padding of group's cards
+    py: 7  // padding of group's cards
 };
 
 sizes.columns = function (group) {
