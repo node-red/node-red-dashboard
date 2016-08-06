fa0d7feeca15c15f0c372d3789177e5ea3941d19
pelis <peter.lindlau@pol-it.de>
2016-08-06 22:37:06 +0200

    Merge PR - allow text-input on CR rather than timeout
    Merge pull request #40 from pelis/pelis-input-text
    issue #31, item 2

---
+++ ui_text_input.js
@@ -0,0 +1,34 @@
+module.exports = function(RED) {
+    var ui = require('../ui')(RED);
+
+    function TextInputNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        var group = RED.nodes.getNode(config.group);
+        if (!group) { return; }
+        var tab = RED.nodes.getNode(group.config.tab);
+        if (!tab) { return; }
+
+        var done = ui.add({
+            node: node,
+            tab: tab,
+            group: group,
+            control: {
+                type: config.delay < 0 ? 'text-input-CR' : 'text-input',
+                label: config.label,
+                mode: config.mode,
+                delay: config.delay,
+                order: config.order,
+                value: '',
+                width: config.width || group.config.width || 6,
+                height: config.height || 1
+            },
+            beforeSend: function (msg) {
+                msg.topic = config.topic || msg.topic;
+            },
+        });
+        node.on("close", done);
+    }
+    RED.nodes.registerType("ui_text_input", TextInputNode);
+};
