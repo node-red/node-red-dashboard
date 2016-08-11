c8262d1557c49933520bdf8363cc48a79a78d71d
Dave Conway-Jones <conway@uk.ibm.com>
2016-08-11 18:19:30 +0100

    Better solution for template height problem

--- src/components/ui-card-panel/ui-card-panel-ctrl.js
+++ src/components/ui-card-panel/ui-card-panel-ctrl.js
@@ -36,11 +36,11 @@ angular.module('ui').controller('uiCardPanelController', ['uiSizes', '$timeout',
                 var result = size.split('x');
                 var width = Math.max(1, Math.min(sizes.columns($scope.group), result ? parseInt(result[0]) || defaultWidth : defaultWidth));
                 var height = Math.max(1, result ? parseInt(result[1]) || defaultHeight : defaultHeight);
-                if (!parseInt(result[1]) /*is template node*/) {
+                if (child.attr('ui-template') !== undefined) { /*is template node*/
                     // template node will size the height based upon it's content
                     // - child.height() defaults to calculating based on width of group
                     var ch = child.height() * parseInt($scope.group.header.config.width)/width;
-                    height = Math.ceil((ch + sizes.cy)/(sizes.cy + sizes.sy)+0.25);
+                    height = Math.ceil((ch + sizes.cy)/(sizes.cy + sizes.sy)+0.1);
                 }
                 var position = getNextPosition(width, height);
                 child.css({
