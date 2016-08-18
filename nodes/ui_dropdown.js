b037bbc7b7dd9055284f7da9d735e3648106076a
Peter Lindlau <Peter Lindlau@POL>
2016-08-18 11:36:02 +0200

    - ui_dropdown.js moved code to convert() - invalid options are now ignored
     Your branch is up-to-date with 'origin/master'.

--- nodes/ui_dropdown.js
+++ nodes/ui_dropdown.js
@@ -20,15 +20,18 @@ module.exports = function(RED) {
                 options: config.options
             };
 
+        var emitOptions;
+
         var done = ui.add({
             node: node,
             tab: tab,
             group: group,
             control: control,
-            
-            beforeEmit: function (msg, newValue) {
+
+            convert: function (payload, oldValue, msg) {
+                // convert msg
                 // as of now, only allow a full replacement of options
-                // beforeEmit is only called when an node linked to us send a msg
+                // beforeEmit is only called when a node linked to us sends a msg
                 // we are expecting to receive an "update options" msg 
                 // which we expect to be an array of new options
 
@@ -36,48 +39,72 @@ module.exports = function(RED) {
                 // that this is an "update options" message coming from the input sender
                 // 'beforeEmit' is called before 'beforeSend', so we may pass in that info
                 // otherwise that convenience info would not be sent (would not cause any problems)...
-                msg.isOptionUpdate = true;
-                msg.isOptionsValid = false; // initial value
+          
+                emitOptions = {isOptionsValid: false, value: undefined, newOptions : undefined};
 
-                if (!Array.isArray(msg.payload)) return {isOptionsValid: false, value: undefined, newOptions : undefined};
+                do {
+                    if (!Array.isArray(msg.payload)) break;
 
-                // an empty array will remove all items from the dropdown 
-                if (msg.payload.length === 0) {
-                    msg.isOptionsValid = true;
-                    control.options = [];
-                    return {isOptionsValid: true, value: undefined, newOptions : []};
-                }
-
-                // just doing rudimentary checks on the input payload...
-                if (msg.payload[0].hasOwnProperty("label") && msg.payload[0].hasOwnProperty("value")) { 
-                    // we expect all objects to contain 'label' and 'value' members
-                    for (i = 0; i < msg.payload.length; i++) {
-                        if (! (msg.payload[i].hasOwnProperty("label") && msg.payload[i].hasOwnProperty("value")) ) break;
+                    emitOptions.newOptions = [];
+                    if (msg.payload.length === 0) {
+                        emitOptions.isOptionsValid = true;
+                        break;
                     }
-                    isOptionsValid = (i === msg.payload.length);
+
+                    // could check whether or not all members have same type
+                    for (var i = 0; i < msg.payload.length; i++) {
+                        var opt = msg.payload[i];
+                        if (opt == undefined || opt == null) continue;
+
+                        switch (typeof opt) {
+                            case 'number':
+                                opt = "" + opt;
+                            case 'string':
+                                emitOptions.newOptions.push({label: opt, value: opt});
+                                break;
+
+                            case 'object':
+                                // assuming array of {label:value}
+                                for (var m in opt) {
+                                    emitOptions.newOptions.push({label: m, value: opt[m]});
+                                    // break after first entry - assuming one entry only
+                                    // might as well support {label1:value1,...,labeln:valuen}  
+                                    break;                                 
+                                }
+                            default:
+                                // do nothing, just continue with next option
+                        }
+                    }
+
+                    if (emitOptions.newOptions.length > 0) emitOptions.value = emitOptions.newOptions[0].value;
+                    if (msg.selectItem) emitOptions.value = msg.selectItem;
+                    emitOptions.isOptionsValid = true;
+
+                } while (false);
+
+                // finally adjust msg to reflect the input
+                msg.isOptionsMsg = true;
+                if (emitOptions.isOptionsValid) {
+                    control.options = emitOptions.newOptions;
+                    control.value = emitOptions.value;
+                    
                 } else {
-                    var newOpts = [];
-                    // we just assume a value array, so we create label / value pairs
-                    for (i = 0; i < msg.payload.length; i++) {
-                        if ( typeof(msg.payload[i]) === 'object' ) break; // invalid
-                        newOpts.push({label: msg.payload[i], value: msg.payload[i]});
-                    }
-                    isOptionsValid = (i === msg.payload.length);
-                    msg.payload = newOpts;
+                    // send error message
+                    node.error("ERR: Invalid Options", msg);
                 }
 
-                // the sending node may pass item to be selected from the new options list
-                // if selectItem does not exist in the options list, dropdown will not select anything
-                if (isOptionsValid) {
-                    value = (msg.selectItem ? msg.selectItem : msg.payload[0].value);
-                    control.options = msg.payload;
-                }
-    
-                msg.isOptionsValid = isOptionsValid;
-                return {isOptionsValid: isOptionsValid, value: value, newOptions: isOptionsValid ? msg.payload : undefined};
+                // we do not ovveride payload here due to 'opt.emitOnlyNewValues' in ui.js
+                // when undefined is returned, msg will not be forwarded
+            
+                return emitOptions.isOptionsValid ? emitOptions : undefined; // always pass entire object (newValue == oldValue)     
+            },
+            
+            beforeEmit: function (msg, newValue) {
+                return emitOptions;
             },
             
             beforeSend: function (msg) {
+                if (msg.isOptionsMsg) msg.payload = emitOptions.value;
                 msg.topic = config.topic;
             }
         });
