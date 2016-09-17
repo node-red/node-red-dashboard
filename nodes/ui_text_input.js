c552c1f79a2d133db1ad9e241fb041170801a7d6
Dave Conway-Jones <conway@uk.ibm.com>
2016-09-17 15:28:31 +0100

    let numeric and text input also control passhtru
    to close #60
    also reword label slightly.

--- nodes/ui_text_input.js
+++ nodes/ui_text_input.js
@@ -14,6 +14,7 @@ module.exports = function(RED) {
             node: node,
             tab: tab,
             group: group,
+            forwardInputMessages: config.passthru,
             control: {
                 type: (config.delay <= 0 ? 'text-input-CR' : 'text-input'),
                 label: config.label,
