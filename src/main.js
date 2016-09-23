388cb7b2341a8c127880e056fa2f316e83d82ed1
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-09-23 16:25:12 +0100

    improved design

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ app.controller('MainController', ['$mdSidenav', '$window', 'UiEvents', '$locatio
         this.selectedTab = null;
         this.loaded = false;
 
-        var chartData = [];
+        // var chartData = [];
 
         this.toggleSidenav = function () {
             $mdSidenav('left').toggle();
@@ -84,56 +84,30 @@ app.controller('MainController', ['$mdSidenav', '$window', 'UiEvents', '$locatio
                 if (msg.hasOwnProperty(key)) {
                     if (key === 'id') { continue; }
 
-                    //(dan): I think this is where the data gets set - yes it is. We need to handle chart data here
-                    //through a switch statement - messy but trying for now.
-                    //Initial data comes in with the key "Data"
-
-                    //If we are dealing with the values of line charts
+                    //If we are dealing with values of line charts
                     if (key === 'value' && found.hasOwnProperty('type') && found['type'] === 'chart' 
                         && found.hasOwnProperty('look') && found['look'] === 'line') {
 
-                        console.log("line chart");
+                        console.log("line chart----");
                         console.log(msg);
-
-                        //add to data storage client side and update the data by updating the found object
-
-                        //find chart by id
-                        var foundChart = false;
-                        var incomingValues = msg.value[0].values;
-                        chartData.forEach(function(chart, chartIndex) {
-                            var id = chart.id;
-                            if (id === msg.id) {
-                                foundChart = true;
-                                incomingValues.forEach(function(incomingValue, valueIndex) {
-                                    chartData[chartIndex].value[0].values.push(incomingValues[valueIndex]);
-                                });
-                            }
-                        });
-
-                        if (!foundChart) {
-                            //add
-                            chartData.push(msg);
-                        }
-                        //find chartIndex for this chartId
-                        var thisChartIndex;
-                        chartData.forEach(function(chart, index) {
-                            if (chart['id'] === found['id']) {
-                                thisChartIndex = index;
-                            }
-                        });
-                        if (!isNaN(thisChartIndex) && thisChartIndex >= 0) {
-                            found[key] = chartData[thisChartIndex][key];
-                        }
                         
+                        //update the value array on the found object to include the new points
+
+                        //If the value object does not exist add the data
+                        if (!found.hasOwnProperty(key) || found.value.length === 0) {
+                            found[key] = msg[key];
+                            
+                        } else {
+                            //otherwise concat the arrays
+                            found[key][0].values = found[key][0].values.concat(msg[key][0].values);
+                        }  
 
                     } else {
-                       found[key] = msg[key];
+                        found[key] = msg[key];
                     }
-
-                    //found[key] = msg[key];
+                    
                 }
             }
-            //found.value = [{key: "Data", values:[[1474463231177,60], [1474463231578,20]]}];
             if (found.hasOwnProperty("me") && found.me.hasOwnProperty("processInput")) {
                 found.me.processInput(msg);
             }
