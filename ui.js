d6af6f7587e3d8a7c64a97bae14153f87baf5a6d
Dave Conway-Jones <conway@uk.ibm.com>
2016-10-11 14:32:19 +0100

    add dashboard version to console log at start

--- ui.js
+++ ui.js
@@ -22,7 +22,8 @@ var serveStatic = require('serve-static'),
     socketio = require('socket.io'),
     path = require('path'),
     fs = require('fs'),
-    events = require('events');
+    events = require('events'),
+    dashboardVersion = require('./package.json').version;
 
 var baseConfiguration = {
     title: "Node-RED Dashboard",
@@ -201,7 +202,7 @@ function init(server, app, log, redSettings) {
         }
     });
 
-    log.info("Dashboard started at " + fullPath);
+    log.info("Dashboard version " + dashboardVersion + " started at " + fullPath);
 
     io.on('connection', function(socket) {
         updateUi(socket);
