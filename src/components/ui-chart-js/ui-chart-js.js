ac8bd77782fa0ffb62665d24a85d518cc960363a
Dan Cunnington <dancunnington@uk.ibm.com>
2016-10-26 16:52:14 +0100

    tidy up

--- src/components/ui-chart-js/ui-chart-js.js
+++ src/components/ui-chart-js/ui-chart-js.js
@@ -12,52 +12,20 @@ angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
                         var type = scope.$eval('me.item.look');
                         return 'components/ui-chart-js/ui-chart-js-'+type+'.html';
                    }
-                    var config = {
-                        colours: [{
-                            backgroundColor: 'rgba(0,0,0,0)',
-                            borderColor: '#A2DED0',
-                            hoverBackgroundColor: '#A2DED0',
-                            hoverBorderColor: '#A2DED0'
-                        }],
-                        data: [[]],
-                        labels: [],
-                        options: {
-                            animation: false,
-                            fill: false,
-                            backgroundColor: "#A2DED0",
-                            scales: {
-                                xAxes: [{
-                                    type: 'time',
-                                    time: {
-                                        displayFormats: {
-                                            'millisecond': 'HH:mm:SS',
-                                            'second': 'HH:mm:SS',
-                                            'minute': 'HH:mm:SS',
-                                            'hour': 'HH:mm:SS',
-                                            'day': 'HH:mm:SS',
-                                            'week': 'HH:mm:SS',
-                                            'month': 'HH:mm:SS',
-                                            'quarter': 'HH:mm:SS',
-                                            'year': 'HH:mm:SS',
-                                          }
-                                    }
-                                }]
-                            }
-                        }
-                    }
-                    scope.config = config;
+                    
+                    scope.config = loadConfiguration();
                     // when new values arrive, update the chart
                     scope.$watch('me.item.value', function (newValue) {
                         if (newValue != undefined) {
                             newValue = newValue[0];
                             //If we are updating, add the data points
                             if (newValue.hasOwnProperty('update') && newValue.update) {
-                                config.labels.push(newValue.values[0].label);
-                                config.data[0].push(newValue.values[0].data);
+                                scope.config.labels.push(newValue.values[0].label);
+                                scope.config.data[0].push(newValue.values[0].data);
                             } else {
                                 //assign all the labels and data to the corresponding arrays
-                                config.labels = newValue.values.labels;
-                                config.data[0] = newValue.values.data;
+                                scope.config.labels = newValue.values.labels;
+                                scope.config.data[0] = newValue.values.data;
                             }
                         }
                     }); 
@@ -66,3 +34,39 @@ angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
         }
     }
 ]);
+
+function loadConfiguration() {
+    return {
+        colours: [{
+            backgroundColor: 'rgba(0,0,0,0)',
+            borderColor: '#A2DED0',
+            hoverBackgroundColor: '#A2DED0',
+            hoverBorderColor: '#A2DED0'
+        }],
+        data: [[]],
+        labels: [],
+        options: {
+            animation: false,
+            fill: false,
+            backgroundColor: "#A2DED0",
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            'millisecond': 'HH:mm:SS',
+                            'second': 'HH:mm:SS',
+                            'minute': 'HH:mm:SS',
+                            'hour': 'HH:mm:SS',
+                            'day': 'HH:mm:SS',
+                            'week': 'HH:mm:SS',
+                            'month': 'HH:mm:SS',
+                            'quarter': 'HH:mm:SS',
+                            'year': 'HH:mm:SS',
+                        }
+                    }
+                }]  
+            }
+        }
+    }
+}
