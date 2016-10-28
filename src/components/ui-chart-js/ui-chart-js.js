3c9163ba75c8982598478d7e06ccea85dff719f6
Dan Cunnington <dancunnington@uk.ibm.com>
2016-10-28 10:00:15 +0100

    tidy up, no fill under lines

--- src/components/ui-chart-js/ui-chart-js.js
+++ src/components/ui-chart-js/ui-chart-js.js
@@ -9,26 +9,26 @@ angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
             link: function(scope, element, attrs) {
                 $timeout(function() {
                     var type = scope.$eval('me.item.look');
+                    const LINE_TYPE = 'line';
+                    const BAR_TYPE = 'bar';
                     scope.getChartTemplateUrl = function() {
                         return 'components/ui-chart-js/ui-chart-js-'+type+'.html';
-                   }
-                                        
+                    }                    
                     scope.config = loadConfiguration(type);
+
                     // when new values arrive, update the chart
                     scope.$watch('me.item.value', function (newValue) {
 
                         if (newValue != undefined && newValue.length > 0) {
                             newValue = newValue[0];
 
-                            if (type === 'line') {
-                                // If we are updating, push to arrays
-                                if (newValue.update) {
-
-                                    //Find the series index
+                            // Updating line charts push to the data arrays
+                            if (type === LINE_TYPE && newValue.update) {
+                                    // Find the series index
                                     var seriesLabel = newValue.key;
                                     var seriesIndex = scope.config.series.indexOf(seriesLabel);
 
-                                    //If it's a new series, add it
+                                    // If it's a new series, add it
                                     if (seriesIndex === -1) {
                                         scope.config.series.push(seriesLabel);
                                         seriesIndex = scope.config.series.indexOf(seriesLabel);
@@ -36,26 +36,20 @@ angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
                                     } 
 
                                     // Ensure the data array is of the correct length
-                                    if (seriesIndex < scope.config.data.length) { scope.config.data.push([]); }
+                                    if (seriesIndex < scope.config.data.length) { scope.config.data.push([]); };
 
                                     // Add the data
                                     scope.config.data[seriesIndex].push(newValue.values.data);
 
-                                } else {
-                                    //Display all data
-                                    scope.config.data = newValue.values.data;
-                                    scope.config.series = newValue.values.series;
-                                }
                             } else {
-                                //bar chart
-                                //Display the new data
-                                if (newValue != undefined) {
-                                    scope.config.data = newValue.values.data;
-                                    scope.config.labels = newValue.values.series;
-                                }
+                                // Bar charts and non update line charts replace the data
+                                scope.config.data = newValue.values.data;
+                                if (type === LINE_TYPE) { scope.config.series = newValue.values.series; };
+                                if (type === BAR_TYPE) { scope.config.labels = newValue.values.series; };
+                                
                             }
                         } else {
-                            //reset data and labels
+                            // Flow deployed - reset data and labels
                             scope.config.data = [];
                             scope.config.labels = [];
                             scope.config.series = [];
@@ -67,29 +61,18 @@ angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
     }
 ]);
 
-// helper function to load the chart configuration
 function loadConfiguration(type) {
-
-    // Set colours
-    var colours = [{
-        backgroundColor: 'rgba(0,0,0,0)',
-        borderColor: '#A2DED0',
-        hoverBackgroundColor: '#A2DED0',
-        hoverBorderColor: '#A2DED0'
-    }, {
-        backgroundColor: 'rgba(0,0,0,0)',
-        borderColor: '#803690',
-        hoverBackgroundColor: '#803690',
-        hoverBorderColor: '#803690'
-    }];
     if (type === 'line') {
         return {
-            colours: colours,
             data: [],
             series: [],
             options: {
                 animation: false,
-                backgroundColor: "#A2DED0",
+                elements: {
+                    line: {
+                        fill: false
+                    }
+                },
                 spanGaps: true,
                 scales: {
                     xAxes: [{
@@ -112,7 +95,7 @@ function loadConfiguration(type) {
             }
         }
     } else {
+        //bar chart returns no configuration
         return {};
     }
-    
 }
