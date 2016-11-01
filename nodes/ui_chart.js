2a80a7c4d2b8452ad0ca4ec2c8d9aef15ffe04da
Dan Cunnington <dancunnington@uk.ibm.com>
2016-11-01 13:25:06 +0000

    Fix for chart node memory leak

--- nodes/ui_chart.js
+++ nodes/ui_chart.js
@@ -6,17 +6,14 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         this.chartType = config.chartType || "line";
         var node = this;
-
         var group = RED.nodes.getNode(config.group);
         if (!group) { return; }
         var tab = RED.nodes.getNode(group.config.tab);
         if (!tab) { return; }
-
         if (config.width === "0") { delete config.width; }
         if (config.height === "0") { delete config.height; }
         // number of pixels wide the chart will be... 43 = sizes.sx - sizes.px
         var pixelsWide = ((config.width || group.config.width || 6) - 1) * 43 - 15;
-        //console.log("pixelsWide",pixelsWide);
 
         if (!tab || !group) { return; }
         var options = {
@@ -34,9 +31,9 @@ module.exports = function(RED) {
                 nodata: config.nodata,
                 width: config.width || group.config.width || 6,
                 height: config.height || parseInt(group.config.width/2+1) || 4,
-                ymin: config.ymin,
+                ymin: config.ymin || 0,
                 ymax: config.ymax,
-                xformat : config.xformat || "%H:%M:%S"
+                xformat : config.xformat || "HH:mm:SS"
             },
             convert: function(value, oldValue, msg) {
                 if (ChartIdList.hasOwnProperty(node.id) && ChartIdList[node.id] !== node.chartType) {
@@ -48,63 +45,119 @@ module.exports = function(RED) {
                     oldValue = value;
                 } else {
                     value = parseFloat(value);
+                    var point;
                     if (isNaN(value)) { return oldValue; }
-                    var topic = msg.topic || 'Data';
+                    var series = msg.topic || 'Series 1';
+                    var storageKey = node.id;
                     var found;
-                    if (!oldValue) { oldValue = []; }
+                    if (!oldValue) { oldValue = [];}
+                    var objectToReturn;
                     if (node.chartType === "bar") {  // handle bar type data
-                        if (oldValue.length === 0) { oldValue = [{ key:node.id, values:[] }] }
-                        for (var j = 0; j < oldValue[0].values.length; j++) {
-                            if (oldValue[0].values[j][0] === topic) {
-                                oldValue[0].values[j][1] = value;
+                        if (oldValue.length == 0) {
+                            oldValue = [{
+                                key: storageKey,
+                                values: {
+                                    data: [],
+                                    series: []
+                                }
+                            }]
+                        }
+                        for (var i=0; i<oldValue[0].values.series.length; i++) {
+                            if (oldValue[0].values.series[i] === series) {
+                                oldValue[0].values.data[i] = value;
                                 found = true;
                                 break;
                             }
                         }
                         if (!found) {
-                            oldValue[0].values.push([topic,value]);
+                            oldValue[0].values.series.push(series);
+                            oldValue[0].values.data.push(value);
+                        }
+                        objectToReturn = {
+                            update: false,
+                            updatedValues: oldValue
                         }
                     }
-                    else { // handle line and area data
+                    else { // Line chart
+
+                        // Find the chart data
                         for (var i = 0; i < oldValue.length; i++) {
-                            if (oldValue[i].key === topic) {
+                            if (oldValue[i].key === storageKey) {
                                 found = oldValue[i];
                                 break;
                             }
                         }
+
+                        // Setup the data structure if this is the first time
                         if (!found) {
-                            found = { key:topic, values:[] };
+                            found = { 
+                                key: storageKey, 
+                                values: {
+                                    series: [],
+                                    data: []
+                                }
+                            }
                             oldValue.push(found);
                         }
-                        var time = new Date().getTime();
-                        var point = [time, value];
-                        found.values.push(point);
 
+                        // Create the new point and add to the dataset
+                        // Create series if it doesn't exist
+                        var seriesIndex = found.values.series.indexOf(series);
+                        if (seriesIndex === -1) {
+                            found.values.series.push(series);
+                            found.values.data.push([]);
+                            seriesIndex = found.values.series.indexOf(series);
+                        }
+
+                        // Add a new point
+                        var time = new Date().getTime();
+
+                        // Add the data to the correct series
+                        var point = {"x": time, "y": value};
+                        found.values.data[seriesIndex].push(point);
+                        
+
+                        // Remove datapoints older than a certain time
                         var limitOffsetSec = parseInt(config.removeOlder) * parseInt(config.removeOlderUnit);
                         var limitTime = new Date().getTime() - limitOffsetSec * 1000;
 
                         var remove = [];
                         oldValue.forEach(function (series, index) {
                             var i=0;
-                            while (i<series.values.length && series.values[i][0]<limitTime) { i++; }
-                            if (i) { series.values.splice(0, i); }
-                            if (series.values.length === 0) { remove.push(index); }
+                            while (i<series.values.data[seriesIndex].length && series.values.data[seriesIndex][i]['x']<limitTime) { i++; }
+                            if (i) { 
+                                series.values.data[seriesIndex].splice(0, i);
+                            }
+                            if (series.values.data[seriesIndex].length === 0) { remove.push(index); }
                         });
 
                         remove.forEach(function (index) {
                             oldValue.splice(index, 1);
                         });
 
-                        // if more datapoints than number of pixels wide...
-                        // TODO - warning is not the answer but hey... it's a hint.
-                        // if (found.values.length % pixelsWide === 0) {
+                        // If more datapoints than number of pixels wide...
+                        // if (found.values.data[seriesIndex].length % pixelsWide === 0) {
                         //     node.warn("More than "+found.values.length+" datapoints");
                         // }
+                        
+                        // Return an object including the new point and all the values
+                        objectToReturn = {
+                            update: true,
+                            newPoint: [{
+                                key: series, 
+                                update: true, 
+                                values: {
+                                    data: point
+                                }
+                            }],
+                            updatedValues: oldValue
+                        }
                     }
                 }
-                return oldValue;
+                return objectToReturn;
             }
         };
+
         var done = ui.add(options);
         setTimeout(function() {
             node.send([null, {payload:"restore", for:node.id}]);
---
+++ src/components/ui-chart-js/ui-chart-js.js
@@ -0,0 +1,132 @@
+/* global JustGage */
+/* global angular */
+angular.module('ui').directive('uiChartJs', [ '$timeout', '$interpolate',
+    function ($timeout, $interpolate) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div ng-include="getChartTemplateUrl()"></div>',
+            link: function(scope, element, attrs) {
+                $timeout(function() {
+                    var type = scope.$eval('me.item.look');
+                    const LINE_TYPE = 'line';
+                    const BAR_TYPE = 'bar';
+                    scope.getChartTemplateUrl = function() {
+                        return 'components/ui-chart-js/ui-chart-js-'+type+'.html';
+                    }                    
+                    scope.config = loadConfiguration(type, scope);
+                   
+                    // When new values arrive, update the chart
+                    scope.$watch('me.item.value', function (newValue) {
+                        if (newValue != undefined && newValue.length > 0) {
+                            scope.config.nodata = false;
+                            newValue = newValue[0];
+
+                            // Updating line charts push to the data arrays
+                            if (type === LINE_TYPE && newValue.update) {
+                                    // Find the series index
+                                    var seriesLabel = newValue.key;
+                                    var seriesIndex = scope.config.series.indexOf(seriesLabel);
+
+                                    // If it's a new series, add it
+                                    if (seriesIndex === -1) {
+                                        scope.config.series.push(seriesLabel);
+                                        seriesIndex = scope.config.series.indexOf(seriesLabel);
+                                        scope.config.data.push([]);
+                                    } 
+
+                                    // Ensure the data array is of the correct length
+                                    if (seriesIndex > scope.config.data.length) { scope.config.data.push([]); };
+
+                                    // Add the data
+                                    scope.config.data[seriesIndex].push(newValue.values.data);
+
+                            } else {
+                                // Bar charts and non update line charts replace the data
+                                scope.config.data = newValue.values.data;
+                                if (type === LINE_TYPE) { scope.config.series = newValue.values.series; };
+                                if (type === BAR_TYPE) { scope.config.labels = newValue.values.series; };
+                                
+                            }
+                        } else {
+                            // Flow deployed - reset config
+                            scope.config.nodata = true; 
+                        }
+                    }); 
+                }, 0);
+            }
+        }
+    }
+]);
+
+function loadConfiguration(type,scope) {
+    var yMin = parseFloat(scope.$eval('me.item.ymin'));
+    var yMax = parseFloat(scope.$eval('me.item.ymax'));
+    var legend = scope.$eval('me.item.legend');
+    var interpolate = scope.$eval('me.item.interpolate');
+    var xFormat = scope.$eval('me.item.xformat');
+    var colours = ['#1F77B4', '#AEC7E8', '#FF7F0E', '#2CA02C', '#98DF8A', '#D62728', '#FF9896', '#9467BD', '#C5B0D5'];
+    var config = {};
+    config.data = [];
+    config.series = [];
+    config.labels = [];
+    config.colours = colours;
+    config.options = {
+        animation: false,
+        spanGaps: true,
+        scales: {},
+        legend: false,
+        responsive: true
+    };
+    
+    if (type === 'line') {
+        config.options.scales.xAxes = [{
+            type: 'time',
+            time: {
+                //override xAxes formats
+                displayFormats: {
+                    'millisecond': xFormat,
+                    'second': xFormat,
+                    'minute': xFormat,
+                    'hour': xFormat,
+                    'day': xFormat,
+                    'week': xFormat,
+                    'month': xFormat,
+                    'quarter': xFormat,
+                    'year': xFormat,
+                }
+            }
+        }];
+        config.options.elements = {
+            line: {
+                fill: false
+            }
+        }
+        switch(interpolate) {
+            case 'linear':
+                config.options.elements.line.tension = 0;
+                break;
+            case 'bezier':
+                config.options.elements.line.tension = 0.4;
+                break;
+            case 'step':
+                config.options.elements.line.stepped = true;
+                break;
+        } 
+    }
+
+    config.options.scales.yAxes = [{}];
+    if (!isNaN(yMin) && !isNaN(yMax)) {
+        config.options.scales.yAxes[0].ticks = {
+            min: yMin,
+            max: yMax
+        }
+    }
+    if (type === 'bar') {
+        config.options.scales.yAxes[0].beginAtZero = true;
+    }
+    if (type === 'line' && JSON.parse(legend)) {
+        config.options.legend = {display: true};
+    }
+    return config;
+}
