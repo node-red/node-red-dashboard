e685c39734653dafca445643b1a921cfe2bcaa79
Dan Cunnington <dancunnington@hotmail.co.uk>
2016-11-09 15:23:44 +0000

    Reimplementation of the chart node using angular-chart.js (#95)
    * Update Dashboard license for JSF
    
    * Reimplement chart node to fix memory leak
    
    * Tidy up comments
    
    * fixup tooltips

--- nodes/ui_chart.js
+++ nodes/ui_chart.js
@@ -6,17 +6,14 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         this.chartType = config.chartType || "line";
         var node = this;
-
         var group = RED.nodes.getNode(config.group);
         if (!group) { return; }
         var tab = RED.nodes.getNode(group.config.tab);
         if (!tab) { return; }
-
         if (config.width === "0") { delete config.width; }
         if (config.height === "0") { delete config.height; }
         // number of pixels wide the chart will be... 43 = sizes.sx - sizes.px
         var pixelsWide = ((config.width || group.config.width || 6) - 1) * 43 - 15;
-        //console.log("pixelsWide",pixelsWide);
 
         if (!tab || !group) { return; }
         var options = {
@@ -34,9 +31,9 @@ module.exports = function(RED) {
                 nodata: config.nodata,
                 width: config.width || group.config.width || 6,
                 height: config.height || parseInt(group.config.width/2+1) || 4,
-                ymin: config.ymin,
+                ymin: config.ymin || 0,
                 ymax: config.ymax,
-                xformat : config.xformat || "%H:%M:%S"
+                xformat : config.xformat || "HH:mm:SS"
             },
             convert: function(value, oldValue, msg) {
                 if (ChartIdList.hasOwnProperty(node.id) && ChartIdList[node.id] !== node.chartType) {
@@ -48,66 +45,132 @@ module.exports = function(RED) {
                     oldValue = value;
                 } else {
                     value = parseFloat(value);
+                    var point;
                     if (isNaN(value)) { return oldValue; }
-                    var topic = msg.topic || 'Data';
+                    var series = msg.topic || 'Series 1';
+                    var storageKey = node.id;
                     var found;
-                    if (!oldValue) { oldValue = []; }
+                    if (!oldValue) { oldValue = [];}
+                    var converted = {};
                     if (node.chartType === "bar") {  // handle bar type data
-                        if (oldValue.length === 0) { oldValue = [{ key:node.id, values:[] }] }
-                        for (var j = 0; j < oldValue[0].values.length; j++) {
-                            if (oldValue[0].values[j][0] === topic) {
-                                oldValue[0].values[j][1] = value;
+                        if (oldValue.length == 0) {
+                            oldValue = [{
+                                key: storageKey,
+                                values: {
+                                    data: [],
+                                    series: []
+                                }
+                            }]
+                        }
+                        for (var i=0; i<oldValue[0].values.series.length; i++) {
+                            if (oldValue[0].values.series[i] === series) {
+                                oldValue[0].values.data[i] = value;
                                 found = true;
                                 break;
                             }
                         }
                         if (!found) {
-                            oldValue[0].values.push([topic,value]);
+                            oldValue[0].values.series.push(series);
+                            oldValue[0].values.data.push(value);
                         }
+                        converted.update = false;
+                        converted.updatedValues = oldValue;
                     }
-                    else { // handle line and area data
+                    else { // Line chart
+
+                        // Find the chart data
                         for (var i = 0; i < oldValue.length; i++) {
-                            if (oldValue[i].key === topic) {
+                            if (oldValue[i].key === storageKey) {
                                 found = oldValue[i];
                                 break;
                             }
                         }
+
+                        // Setup the data structure if this is the first time
                         if (!found) {
-                            found = { key:topic, values:[] };
+                            found = { 
+                                key: storageKey, 
+                                values: {
+                                    series: [],
+                                    data: []
+                                }
+                            }
                             oldValue.push(found);
                         }
-                        var time = new Date().getTime();
-                        var point = [time, value];
-                        found.values.push(point);
 
+                        // Create the new point and add to the dataset
+                        // Create series if it doesn't exist
+                        var seriesIndex = found.values.series.indexOf(series);
+                        if (seriesIndex === -1) {
+                            found.values.series.push(series);
+                            found.values.data.push([]);
+                            seriesIndex = found.values.series.indexOf(series);
+                        }
+
+                        // Add a new point
+                        var time = new Date().getTime();
+
+                        // Add the data to the correct series
+                        var point = {"x": time, "y": value};
+                        found.values.data[seriesIndex].push(point);
+                        
+
+                        // Remove datapoints older than a certain time
                         var limitOffsetSec = parseInt(config.removeOlder) * parseInt(config.removeOlderUnit);
                         var limitTime = new Date().getTime() - limitOffsetSec * 1000;
+                        var pointsLimit = config.removeOlderPoints;
+                        var removed = [];
+                        var removeSeries = [];
 
-                        var remove = [];
-                        oldValue.forEach(function (series, index) {
-                            var i=0;
-                            while (i<series.values.length && series.values[i][0]<limitTime) { i++; }
-                            if (i) { series.values.splice(0, i); }
-                            if (series.values.length === 0) { remove.push(index); }
+                        oldValue[0].values.data.forEach(function(series, seriesIndex) {
+                            var i = 0;
+                            while (i < series.length && series[i]['x'] < limitTime) { i++; }
+                            if (i > 0) { 
+                                series.splice(0, i);
+                                removed.push({seriesIndex: seriesIndex, noPoints: i});
+                            }
+
+                            // Remove old datapoints if total is greater than points limit
+                            if (pointsLimit > 0 && series.length > pointsLimit) {
+                                var noToRemove = series.length - pointsLimit;
+                                series.splice(0, noToRemove);
+                                removed.push({seriesIndex: seriesIndex, noPoints: noToRemove});
+                            }
+
+                            if (series.length === 0) { removeSeries.push(seriesIndex); }
                         });
 
-                        remove.forEach(function (index) {
-                            oldValue.splice(index, 1);
+                        // Ensure series match up
+                        removeSeries.forEach(function(index) {
+                            oldValue[0].values.series.splice(index, 1);
+                            oldValue[0].values.data.splice(index, 1);
                         });
 
-                        // if more datapoints than number of pixels wide...
-                        // TODO - warning is not the answer but hey... it's a hint.
-                        // if (found.values.length % pixelsWide === 0) {
+                        // If more datapoints than number of pixels wide...
+                        // if (found.values.data[seriesIndex].length % pixelsWide === 0) {
                         //     node.warn("More than "+found.values.length+" datapoints");
                         // }
+                        
+                        // Return an object including the new point and all the values
+                        converted.update = true;
+                        converted.newPoint = [{
+                            key: series, 
+                            update: true,
+                            removedData: removed,
+                            removedSeries: removeSeries,
+                            values: {
+                                data: point
+                            }
+                        }];
+                        converted.updatedValues = oldValue;
                     }
                 }
-                return oldValue;
+                return converted;
             }
         };
+
         var done = ui.add(options);
         setTimeout(function() {
-            node.send([null, {payload:"restore", for:node.id}]);
             node.emit("input",{payload:"start"}); // trigger a redraw at start to flush out old data.
         }, 100);
         node.on("close", done);
