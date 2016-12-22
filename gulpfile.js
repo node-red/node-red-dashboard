e51d680357664f96750842a0f743b2e512e0161d
Dan Cunnington <dancunnington@uk.ibm.com>
2016-12-22 14:39:05 +0000

    gulp fixes and file include is dynamic to dashboard /ui path

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,13 @@ var
     gulpif = require('gulp-if'),
     header = require("gulp-header"),
     htmlreplace = require('gulp-html-replace'),
+    insertLines = require('gulp-insert-lines'),
     manifest = require('gulp-manifest'),
     minifyCss = require('gulp-clean-css'),
     minifyHTML = require('gulp-htmlmin'),
     path = require('path'),
+    resources = require('gulp-resources'),
+    removeHtml = require('gulp-remove-html');
     spawn = require('child_process').spawn,
     streamqueue = require('streamqueue'),
     templateCache = require('gulp-angular-templatecache'),
@@ -21,7 +24,7 @@ var
 
 gulp.task('default', ['manifest']);
 
-gulp.task('build', ['icon', 'js', 'css', 'index', 'fonts']);
+gulp.task('build', ['icon', 'js', 'css', 'less', 'index', 'fonts']);
 
 gulp.task('publish', ['build'], function (done) {
     spawn('npm', ['publish'], { stdio:'inherit' }).on('close', done);
@@ -56,7 +59,11 @@ gulp.task('index', function() {
     return gulp.src('src/index.html')
     .pipe(htmlreplace({
         'css': 'css/app.min.css',
-        'js': 'js/app.min.js'
+        'js': 'js/app.min.js',
+    }))
+    .pipe(insertLines({
+      'before': /<\/head>$/,
+      'lineBefore': '<link rel="stylesheet/less" href="css/app.min.less" />'
     }))
     .pipe(minifyHTML({collapseWhitespace:true, conservativeCollapse:true}))
     .pipe(gulp.dest('dist/'));
@@ -93,9 +100,18 @@ gulp.task('css', function () {
     return gulp.src('src/index.html')
     .pipe(ghtmlSrc({getFileName: getFileName.bind(this, 'href'), presets: 'css'}))
     .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(concat('app.min.less'))
+    .pipe(concat('app.min.css'))
     .pipe(header(fs.readFileSync('license.js')))
     .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('less', function() {
+    return gulp.src('src/index.html')
+    .pipe(resources({less: true, css: false, js: false}))
+    .pipe(removeHtml())
+    .pipe(gulpif('**/*.less', concat('app.min.less')))
+    .pipe(header(fs.readFileSync('license.js')))
+    .pipe(gulp.dest('dist/css'));
 });
 
 var vendorPrefix = "vendor/";
