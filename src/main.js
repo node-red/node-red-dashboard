9acd22a47ec3d7179b3a2564a0c3c7782cd16064
Dan Cunnington <dancunnington@hotmail.co.uk>
2017-01-03 15:31:52 +0000

    [themes] fix for colours on safari and backwards compatibility for light and dark theme (#140)
    * fix for colours on safari
    
    * backwards compatability
    
    * remove old base node when upgrading
    
    * preserve title and theme choice when upgrading

--- src/main.js
+++ src/main.js
@@ -91,7 +91,35 @@ app.controller('MainController', ['$mdSidenav', '$window', 'UiEvents', '$locatio
                 if (main.selectedTab && typeof(main.selectedTab.theme) === 'object') {
                     if (main.selectedTab.theme.themeState['base-color'].value) {
                         applyStyle(main.selectedTab.theme); 
-                    } 
+                    } else {
+                        //apply the correct theme - backwards compatability
+                        var themeSettings = {themeState: {}};
+                        if (main.selectedTab.theme.name === 'theme-dark') {
+                            themeSettings.themeState["base-color"] = {value: "#097479"};
+                            themeSettings.themeState["page-backgroundColor"] = {value: "#111111"};
+                            themeSettings.themeState["page-titlebar-backgroundColor"] = {value: "#097479"};
+                            themeSettings.themeState["page-sidebar-backgroundColor"] = {value: "#000000"};
+                            themeSettings.themeState["group-backgroundColor"] = {value: "#333333"};
+                            themeSettings.themeState["group-textColor"] = {value: "#10cfd8"};
+                            themeSettings.themeState["group-borderColor"] = {value: "#555555"};
+                            themeSettings.themeState["widget-textColor"] = {value: "#eeeeee"};
+                            themeSettings.themeState["widget-backgroundColor"] = {value: "#097479"};
+
+                        } else {
+                            //light theme backwards compatability
+                            themeSettings.themeState["base-color"] = {value: "#0094CE"};
+                            themeSettings.themeState["page-backgroundColor"] = {value: "#fafafa"};
+                            themeSettings.themeState["page-titlebar-backgroundColor"] = {value: "#0094CE"};
+                            themeSettings.themeState["page-sidebar-backgroundColor"] = {value: "#ffffff"};
+                            themeSettings.themeState["group-backgroundColor"] = {value: "#ffffff"};
+                            themeSettings.themeState["group-textColor"] = {value: "#000000"};
+                            themeSettings.themeState["group-borderColor"] = {value: "#ffffff"};
+                            themeSettings.themeState["widget-textColor"] = {value: "#111111"};
+                            themeSettings.themeState["widget-backgroundColor"] = {value: "#0094CE"};
+                        }
+                        applyStyle(themeSettings);
+                        
+                    }
                     $mdToast.hide();
                     done();
                 }
