4c4d1b85d76dfae38c36a60e7d741b71d5b25b70
Dave Conway-Jones <conway@uk.ibm.com>
2017-02-26 17:20:21 +0000

    stop button forwarding message
    as the only input can be enable/disable
    to close #170

--- nodes/ui_button.js
+++ nodes/ui_button.js
@@ -33,6 +33,7 @@ module.exports = function(RED) {
             node: node,
             tab: tab,
             group: group,
+            forwardInputMessages: false,
             control: {
                 type: 'button',
                 label: config.label,
