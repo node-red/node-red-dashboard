92188d2dcbce5d0353d5bce4794204c4173cfdd3
Dave Conway-Jones <conway@uk.ibm.com>
2017-11-14 20:44:44 +0000

    add id to more  elements

---
+++ src/filters/spaceToUnderscore.js
@@ -0,0 +1,11 @@
+angular.module('ui').filter('spaceToUnderscore', function() {
+    return function (input) {
+        return input.replace(/ /g,'_');
+    };
+});
+
+angular.module('ui').filter('underscoreToSpace', function() {
+    return function (input) {
+        return input.replace(/_/g,' ');
+    };
+});
