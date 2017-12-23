a5556c7534a2e2b7c9e43dc5b557c45349513251
Hugobox <hugobox@gmail.com>
2017-12-23 07:16:40 -0500

    Dashboard Card collapse/expand (#333)

--- src/components/ui-card-panel/ui-card-panel.js
+++ src/components/ui-card-panel/ui-card-panel.js
@@ -1,4 +1,5 @@
 /* global angular */
+
 angular.module('ui').directive('uiCardPanel', [
     function() {
         return {
@@ -7,9 +8,17 @@ angular.module('ui').directive('uiCardPanel', [
             templateUrl: 'components/ui-card-panel/ui-card-panel.html',
             controller: 'uiCardPanelController',
             controllerAs: 'ctrl',
+
             link: function (scope, element, attrs, controller) {
                 var root = element.find(".nr-dashboard-cardcontainer");
                 controller.init(root);
+				
+				scope.collapseCard = function () {
+				var slideDuration = parseInt(attrs.slideToggleDuration, 10) || 500;
+				root.slideToggle(slideDuration);
+				setTimeout(function() {$(window).trigger('resize');}, 600);
+				}
             }
         };
-    }]);
+    }
+]);
