5515b80b4795999d984b7509f03d93a3a4119fb0
Dave Conway-Jones <conway@uk.ibm.com>
2017-12-23 12:49:54 +0000

    Tidy up group collapse and make optional
    to close #73 and close #177
    Thanks @hugobox for the hard work

--- src/components/ui-card-panel/ui-card-panel.js
+++ src/components/ui-card-panel/ui-card-panel.js
@@ -8,16 +8,14 @@ angular.module('ui').directive('uiCardPanel', [
             templateUrl: 'components/ui-card-panel/ui-card-panel.html',
             controller: 'uiCardPanelController',
             controllerAs: 'ctrl',
-
             link: function (scope, element, attrs, controller) {
                 var root = element.find(".nr-dashboard-cardcontainer");
                 controller.init(root);
-				
-				scope.collapseCard = function () {
-				var slideDuration = parseInt(attrs.slideToggleDuration, 10) || 500;
-				root.slideToggle(slideDuration);
-				setTimeout(function() {$(window).trigger('resize');}, 600);
-				}
+                scope.collapseCard = function () {
+                    var slideDuration = parseInt(attrs.slideToggleDuration, 10) || 200;
+                    root.slideToggle(slideDuration);
+                    setTimeout(function() {$(window).trigger('resize');}, 200);
+                }
             }
         };
     }
