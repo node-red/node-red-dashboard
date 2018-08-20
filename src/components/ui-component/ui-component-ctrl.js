5835a28a1b67a707c69c7090bbcb6612af58a0d9
Dave Conway-Jones <conway@uk.ibm.com>
2018-08-20 09:46:18 +0100

    Adjust slider timing

--- src/components/ui-component/ui-component-ctrl.js
+++ src/components/ui-component/ui-component-ctrl.js
@@ -283,13 +283,15 @@ angular.module('ui').controller('uiComponentController', ['$scope', 'UiEvents', 
                 events.emit(data);
                 return;
             }
-            if (timer) {
-                clearTimeout(timer);
+            // if (timer) {
+            //     clearTimeout(timer);
+            // }
+            if (!timer) {
+                timer = setTimeout(function () {
+                    timer = undefined;
+                    events.emit(data);
+                }, timeout);
             }
-            timer = setTimeout(function () {
-                timer = undefined;
-                events.emit(data);
-            }, timeout);
         };
 
         // may add additional input processing for other controls
