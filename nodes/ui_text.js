ec8fa06c90a103432d6b62fbb63f9a234fd65344
Hugobox <hugobox@gmail.com>
2018-11-20 10:17:47 -0500

    Update ui_text.js

--- nodes/ui_text.js
+++ nodes/ui_text.js
@@ -26,6 +26,7 @@ module.exports = function(RED) {
             control: {
                 type: 'text',
                 label: config.label,
+                tooltip: config.tooltip,
                 order: config.order,
                 format: config.format,
                 width: config.width || group.config.width || 6,
