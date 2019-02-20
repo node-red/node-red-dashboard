27ffc109ac44e777ab1eb864ae82d06db35f4243
Steve Rickus <srickus@gmail.com>
2019-02-19 23:28:58 -0500

    Allow ui_control through msg.payload.tabs
    Dashboard tabs can be shown/hidden and enabled/disabled by sending
    control options. Changes take effect immediately within the browser, but
    are not yet persisted at the server.

--- nodes/ui_ui_control.js
+++ nodes/ui_ui_control.js
@@ -7,10 +7,15 @@ module.exports = function(RED) {
 
         this.on('input', function(msg) {
             if (typeof msg.payload !== "object") { msg.payload = {tab:msg.payload}; }
+            // show/hide or enable/disable tabs
+            if (msg.payload.hasOwnProperty("tabs")) {
+                ui.emit('ui-control', {tabs:msg.payload.tabs, socketid:msg.socketid});
+            }
             // switch to tab name (or number)
             if (msg.payload.hasOwnProperty("tab")) {
                 ui.emit('ui-control', {tab:msg.payload.tab, socketid:msg.socketid});
             }
+            // show or hide ui groups
             if (msg.payload.hasOwnProperty("group")) {
                 ui.emit('ui-control', {group:msg.payload.group, socketid:msg.socketid});
             }
