7a1c20c574297c0f07658377a38e1a3af9a43177
Dave Conway-Jones <conway@uk.ibm.com>
2019-06-16 14:38:40 +0100

    Fix gauge oddness, fix widget background-color

--- fixfa.js
+++ fixfa.js
@@ -22,7 +22,8 @@ fs.readFile("node_modules/justgage/justgage.js", 'utf8', function (err, file) {
     else {
         console.log("Fixing up JustGage.js");
         var res1 = file.replace(/var alpha, Ro, Ri, Cx, Cy, Xo, Yo, Xi, Yi, path;\n\n/, 'var alpha, Ro, Ri, Cx, Cy, Xo, Yo, Xi, Yi, path;\n\tif (min < 0) {max -= min; value -= min; min = 0; }\n\n');
-        fs.writeFile("node_modules/justgage/justgage.js", res1, 'utf8', function (err) {
+        var res2 = res1.replace(/value > \(\(max - min\)/g,'value - min > ((max - min)');
+        fs.writeFile("node_modules/justgage/justgage.js", res2, 'utf8', function (err) {
             if (err) { console.log("Failed to re-write file."); }
             else {
                 console.log("Fixed  up JustGage.js");
