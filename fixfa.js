78f7dd4bc8d8fd94d58cb5b5ab2b57b8ba90fba5
Dave Conway-Jones <conway@uk.ibm.com>
2019-11-13 11:28:14 +0000

    Add smaller sizes to fa modifiers
    add fa-sm and fa-xs

--- fixfa.js
+++ fixfa.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// Patch to fix Font-Awesome urls for loading font
+// and to add in fa-sm(all) and fa-xs(extra small) modifiers
 var fs = require("fs");
 fs.readFile("node_modules/font-awesome/css/font-awesome.css", 'utf8', function (err, file) {
     if (err) { return; }
@@ -7,7 +9,12 @@ fs.readFile("node_modules/font-awesome/css/font-awesome.css", 'utf8', function (
         console.log("Fixing up Font-Awesome css");
         var res1 = file.replace(/\?v=4\.7\../g, '');
         var res2 = res1.replace(/\&v=4\.7\../g, '');
-        fs.writeFile("node_modules/font-awesome/css/font-awesome.css", res2, 'utf8', function (err) {
+        var res3;
+        if (res2.indexOf("fa-sm ") === -1) {
+            res3 = res2.replace(/fa-lg/, 'fa-sm {font-size:0.875em;}\n.fa-xs {font-size:0.75em;}\n.fa-lg');
+        }
+        else { res3 = res2; }
+        fs.writeFile("node_modules/font-awesome/css/font-awesome.css", res3, 'utf8', function (err) {
             if (err) { console.log("Failed to re-write file."); }
             else {
                 console.log("Fixed  up Font-Awesome css");
